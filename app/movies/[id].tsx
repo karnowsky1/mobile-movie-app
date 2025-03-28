d488dec7412571447df45ba41823338175a4c2fd
karnowsky1 <kestime9015@bths.edu>
2025-03-28 17:46:15 -0400

    feat(app): scaffold React Native movie app with navigation
    - Set up the initial project structure
    - Created a navbar for navigation between pages
    - Implemented tab-based navigation to allow users to switch pages

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieDetiails = () => {
+  return (
+    <View>
+      <Text>MovieDetiails</Text>
+    </View>
+  )
+}
+
+export default MovieDetiails
